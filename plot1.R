4aaa1d63c36920e1b219ecfe8dd3080b472ed49e
vsjagadhish <64471486+vsjagadhish@users.noreply.github.com>
2020-12-15 07:02:29 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library("data.table")
+powerDT <- data.table::fread(input = "household_power_consumption.txt",na.strings="?")
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+png("plot1.png", width=480, height=480)
+hist(powerDT[, Global_active_power], main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,8 @@
+library("data.table")
+powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+plot(x = powerDT[, dateTime], y = powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
